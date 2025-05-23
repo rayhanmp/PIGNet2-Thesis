f060669d57a46ea43706ddc36aacc6d15f4732f6
rayhanmp <rayhanmpramanda@gmail.com>
2025-05-23 15:53:35 +0700

    fix directory path on start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "[SETUP] Installing Python requirements"; \
-conda run -n pignet2 pip install --no-cache-dir -r /PIGNet2/requirements.txt && echo "[SETUP] Requirements installed"; \
+conda run -n pignet2 pip install --no-cache-dir -r requirements.txt && echo "[SETUP] Requirements installed"; \
 echo "[SETUP] Downloading dataset"; \
-bash /PIGNet2/dataset/download.sh && echo "[SETUP] Download completed"; \
-bash /PIGNet2/dataset/untar.sh && echo "[SETUP] Decompression completed"
+bash /dataset/download.sh && echo "[SETUP] Download completed"; \
+bash /dataset/untar.sh && echo "[SETUP] Decompression completed"
