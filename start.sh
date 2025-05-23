a70b6e2e9adc8b707c2c4c4d729e94f692fb09f9
rayhanmp <rayhanmpramanda@gmail.com>
2025-05-23 16:11:45 +0700

    modify start.sh to also run training script

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 echo "[SETUP] Installing Python requirements"; \
 conda run -n pignet2 pip install --no-cache-dir -r requirements.txt && echo "[SETUP] Requirements installed"; \
 echo "[SETUP] Downloading dataset"; \
-bash /dataset/download.sh && echo "[SETUP] Download completed"; \
-bash /dataset/untar.sh && echo "[SETUP] Decompression completed"
+bash dataset/download.sh && echo "[SETUP] Download completed"; \
+bash dataset/untar.sh && echo "[SETUP] Decompression completed" \
+bash experiments/training_scripts/pda_nda.sh && echo "[SETUP] Training completed"; \
+
